3453a6ed34dea252efe53bd2cfabf686233f28c4
Gabriel Rodrigo <pdktche-pb@live.com>
2022-11-16 20:25:41 -0300

    Foi feito o exercicio 2.1.1

---
+++ 2.1.1.js
@@ -0,0 +1,9 @@
+let soma = 0
+let subtraçao = 0
+let divisao = 0
+let modulo = 0
+soma =  2 + 3
+subtraçao = soma - 3
+divisao = 80/3
+modulo = divisao%divisao
+console.log("soma = ",soma, "subtraçao =", subtraçao , "divisao = ", divisao, "modulo = ", modulo)
